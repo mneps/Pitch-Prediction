eb876b33086a147460bce47051ea6ff1dac04721
Matthew Carrington-Fair <matthew.carrington_fair@tufts.edu>
2019-03-09 11:51:02 -0500

    Adding FrequencyTable class

---
+++ src/FrequencyTable.py
@@ -0,0 +1,37 @@
+import sys
+from itertools import permutations
+
+class FrequencyTable(object):
+    def __init__(self, list):
+        self.list = list
+        self.PITCH_TYPES = self.__findPitchTypes()
+        self.convertPitches = self.__initPitchMap()
+        self.freqTable = [ [0 for _ in range(len(self.PITCH_TYPES))] for _ in range(len(self.PITCH_TYPES) ** 2)]
+        self.__populate()
+        
+    def __findPitchTypes(self):
+        pitchTypes = []
+        for atBat in self.list:
+            for pitch in atBat:
+                if pitch not in pitchTypes:
+                    pitchTypes.append(pitch)
+        return pitchTypes
+
+    def __initPitchMap(self):
+        perm = permutations(self.PITCH_TYPES, 2)
+        convertPitches = list(perm)
+        return convertPitches
+    
+    def __populate(self):
+        for atBat in self.list:
+            if len(atBat) < 3:
+                continue
+            prePitches = (atBat[0], atBat[1])
+            col = self.convertPitches.index(prePitches)
+            row = self.PITCH_TYPES.index(atBat[2])
+            self.freqTable[col][row] += 1
+
+            
+            
+    
+        
