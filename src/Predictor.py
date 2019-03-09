9dd8fcf34a77e059f522b452a92f96ccf6823917
Matthew Carrington-Fair <matthew.carrington_fair@tufts.edu>
2019-03-09 15:30:17 -0500

    Using linklength

--- src/Predictor.py
+++ src/Predictor.py
@@ -1,11 +1,12 @@
 import random
+from GlobalVars import *
 from FrequencyTable import *
 
 class Predictor():
 
 	def __init__(self, freqTable, testData):
 		self.freqTable = freqTable
-		self.testData = list(filter(lambda x: len(x) > 2, testData))
+		self.testData = list(filter(lambda x: len(x) > LINK_LENGTH, testData))
 		self.accuracy = self.predictPitches()
 
 
@@ -14,11 +15,10 @@ class Predictor():
 		totalPitches = 0
 
 		for atBat in self.testData:
-			for i in range(2, len(atBat)):
-				prevPitches = (atBat[i-2], atBat[i-1])
-				averages = self.freqTable.lookup(prevPitches)
-				
-				max_avg = 0
+			for i in range(LINK_LENGTH, len(atBat)):
+                                prevPitches = tuple(atBat[i-LINK_LENGTH:i])
+                                averages = self.freqTable.lookup(prevPitches)
+                                max_avg = 0
 				for j in range(len(averages)):
 					(pitch, weight) = averages[j]
 					if weight > max_avg:
