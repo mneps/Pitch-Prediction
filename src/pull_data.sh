903626ce6165e887c0cd74ab522512277c088d4b
Matthew Epstein <matepste@publicisgroupe.net>
2019-03-09 10:55:55 -0500

    bash script concats csv files

--- src/pull_data.sh
+++ src/pull_data.sh
@@ -4,3 +4,9 @@ while IFS='' read -r name || [[ -n "$name" ]]; do
 	echo $name
  	eval python3 pull_data.py $name
 done < "$1"
+
+files=( *.csv ) # collect input filenames in an array
+{
+  head -n 2 "${files[0]}" # output the header lines (using the 1st file)
+  tail -q -n +3 "${files[@]}" # append the data lines from all files, in sequence
+}  > out.csv
